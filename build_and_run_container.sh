3ff6b4fa82eb0d64b94260f3374920e68d11867a
Juan <juanmontalvobressi@hotmail.com>
2023-04-02 21:05:10 -0300

    Created separate logic for deployment in arm via dockerfile

--- build_and_run_container.sh
+++ build_and_run_container.sh
@@ -3,13 +3,23 @@
 CONTAINER_NAME="cont-telegram-whisper-bot"
 IMAGE_NAME="img-telegram-whisper-bot"
 
+# Detect platform architecture
+ARCH=$(uname -m)
+if [[ "$ARCH" == "arm"* ]]; then
+  echo "Detected ARM architecture"
+  DOCKERFILE=Dockerfile_arm
+else
+  echo "Detected x64 architecture"
+  DOCKERFILE=Dockerfile
+fi
+
 # Remove the container if it already exists
 if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
   docker stop $CONTAINER_NAME
   docker rm $CONTAINER_NAME
 fi
 
-# Build the Docker image
-docker build -t $IMAGE_NAME .
+# Build the Docker image with selected Dockerfile
+docker build -t $IMAGE_NAME -f $DOCKERFILE .
 
 docker run -d --name $CONTAINER_NAME --env-file .env $IMAGE_NAME
