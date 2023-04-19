d81d7a169efb313d754a526b1e95b1ba48c14531
Juan Montalvo Bressi <juan-m12i@users.noreply.github.com>
2023-04-19 19:33:03 -0300

    Added GPT-4 compatibility and some comments

--- main.py
+++ main.py
@@ -8,38 +8,48 @@ from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTyp
 from gpt_adapter import OpenAI
 import requests
 
-load_dotenv()
+load_dotenv()  # Python module to load environment variables from a .env file
 
+# Configure logging (native python library)
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-my_open_ai = OpenAI()
-allowed_chat_ids: List[int] = [int(chat_id) for chat_id in os.getenv('ALLOWED_CHAT_IDS').split(',')]
+my_open_ai = OpenAI()  # OpenAI is a custom class that works as a wrapper/adapter for OpenAI's GPT API
+allowed_chat_ids: List[int] = [int(chat_id) for chat_id in os.getenv('ALLOWED_CHAT_IDS').split(',')]  # Pythonic way of creating a list, behaves like a loop
+
+
+# extracted method to only allow verified users
+def allowed_user(update: Update) -> bool:
+    return update.effective_chat.id in allowed_chat_ids
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.id in allowed_chat_ids:
+    if allowed_user(update):
         await update.message.reply_text("Welcome, I'd love to help")
 
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.id in allowed_chat_ids:
+    if allowed_user(update):
         logging.info(f"Replying message from verified user")
         received_message_text = update.message.text
         if received_message_text == 'ping':
             await context.bot.send_message(chat_id=update.effective_chat.id, text="pong")
+        elif received_message_text[:4] == "gpt4":
+            response = my_open_ai.answer_message(received_message_text[4:], model="gpt-4")
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
         elif received_message_text[:3] == "gpt":
             response = my_open_ai.answer_message(received_message_text[4:])
             await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
         else:
             # reply with welcome message
-            await update.message.reply_text("Welcome, I'd love to help")
+            await update.message.reply_text("I don't know how to answer to that")
 
 
+#  This method will be called each time the bot receives an audio file
 async def process_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_chat.id in allowed_chat_ids:
+    if allowed_user(update):
         logging.info(f"Transcribing audio file from verified user")
         audio_file = update.message.audio  # Access the audio file
 
@@ -90,14 +100,18 @@ async def process_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
+    # This comes directly from the telegram bot library
+    bot = ApplicationBuilder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
 
+    # The telegram bot manages events to process through handlers:
+    # For each handled event group, the relevant function (defined above) will be invoked
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), reply)
     audio_handler = MessageHandler(filters.AUDIO, process_audio)
 
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-    application.add_handler(audio_handler)
+    bot.add_handler(start_handler)
+    bot.add_handler(echo_handler)
+    bot.add_handler(audio_handler)
 
-    application.run_polling()
+    # The app will be running constantly checking for new events
+    bot.run_polling()
