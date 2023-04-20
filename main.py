cb1e23f0107dcbbe5a94629c54e9b66a7bb08144
Juan <juan@tymit.com>
2023-04-19 21:26:06 -0300

    refactored whisper into a separate module

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import logging
-import re
 from typing import List
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 from gpt_adapter import OpenAI
-import requests
+
+from whisper_adapter import transcribe_audio_file
 
 load_dotenv()  # Python module to load environment variables from a .env file
 
@@ -59,36 +59,7 @@ async def process_audio(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await file.download_to_drive(local_file_path)  #
 
         # Send the audio file to the OpenAI API endpoint
-        openai_api_key = os.getenv("OPEN_AI_API_KEY")
-        headers = {
-            "Authorization": f"Bearer {openai_api_key}",
-        }
-        url = "https://api.openai.com/v1/audio/transcriptions"
-        model = "whisper-1"
-
-        with open(local_file_path, "rb") as audio_data:
-            files = {"file": (local_file_path, audio_data)}
-            data = {"model": model}
-            response = requests.post(url, headers=headers, data=data, files=files)
-
-        response_text = response.json()["text"]
-
-        # Split the response text into sentences using a regular expression
-        sentences = re.split("(?<=[.!?]) +", response_text)
-
-        # Combine the sentences into messages of no more than 1000 characters each
-        messages = []
-        current_message = ""
-        for sentence in sentences[:-1]:
-            if len(current_message) + len(sentence) <= 1000:
-                current_message += sentence
-            else:
-                messages.append(current_message)
-                current_message = sentence
-        if len(current_message) > 1000:
-            messages.append(current_message)
-            current_message = ""
-        messages.append(current_message + sentences[-1])
+        messages = transcribe_audio_file(local_file_path)
 
         # Send each message as a separate message
         for message in messages:
