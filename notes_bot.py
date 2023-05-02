a1844ce7d64d179dde6aaee9c5cfc3b3859d0b24
Juan <juan@tymit.com>
2023-05-02 13:10:46 -0300

    refactoring bot

--- notes_bot.py
+++ notes_bot.py
@@ -5,15 +5,10 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 from adapters.notion_adapter import NotionAdapter
-
+from common_bot import allowed_user, build_bot, add_handlers
 
 load_dotenv()  # Python module to load environment variables from a .env file
 
-# Configure logging (native python library)
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 
 allowed_chat_ids: List[int] = [int(chat_id) for chat_id in os.getenv('ALLOWED_CHAT_IDS').split(
     ',')]  # Pythonic way of creating a list, behaves like a loop
@@ -38,16 +33,22 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if received_message_text == 'ping':
             await context.bot.send_message(chat_id=update.effective_chat.id, text="pong")
         else:
-            my_notion.add_block(received_message_text)
-            # reply with welcome message
-            await update.message.reply_text("Note stored")
+            try:
+                my_notion.add_block(parent_id=NOTION_PAGE_ID, text=received_message_text, block_type="bulleted_list_item")
+                # reply with success message
+                await update.message.reply_text("Note stored")
+            except Exception as e:
+                logging.error(f"Error while adding block to Notion: {e}")
+                await update.message.reply_text(f"Failed to store note: {e}")
+
+
 
 
 # Remove the temporary audio file
 
 
 if __name__ == '__main__':
-    my_notion.add_block(parent_id=NOTION_PAGE_ID, text="Hello World")
+    my_notion.add_block(parent_id=NOTION_PAGE_ID, text="Hello Worlds", block_type="bulleted_list_item")
 
     # This comes directly from the telegram bot library
     bot = ApplicationBuilder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
