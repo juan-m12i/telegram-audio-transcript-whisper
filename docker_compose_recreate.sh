443c6d7ca0efce5cb05ce1914226878bbfb1b384
Juan <juan@tymit.com>
2025-01-19 15:32:59 -0300

    docker compose recreate for single service

--- docker_compose_recreate.sh
+++ docker_compose_recreate.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 DOCKER_COMPOSE_FILE="docker_compose.yml"
 
-#docker stop cont-telegram-whisper-bot
-#docker rm cont-telegram-whisper-bot
-
-
-docker-compose -f "$DOCKER_COMPOSE_FILE" down
-docker-compose -f "$DOCKER_COMPOSE_FILE" build
-docker-compose -f "$DOCKER_COMPOSE_FILE" up -d --force-recreate
+if [ "$#" -eq 0 ]; then
+  echo "No specific containers provided. Recreating all containers..."
+  docker-compose -f "$DOCKER_COMPOSE_FILE" down
+  docker-compose -f "$DOCKER_COMPOSE_FILE" build
+  docker-compose -f "$DOCKER_COMPOSE_FILE" up -d --force-recreate
+else
+  echo "Recreating specified containers: $@"
+  for SERVICE in "$@"; do
+    echo "Recreating $SERVICE..."
+    docker-compose -f "$DOCKER_COMPOSE_FILE" stop "$SERVICE"
+    docker-compose -f "$DOCKER_COMPOSE_FILE" rm -f "$SERVICE"
+    docker-compose -f "$DOCKER_COMPOSE_FILE" build "$SERVICE"
+    docker-compose -f "$DOCKER_COMPOSE_FILE" up -d --force-recreate "$SERVICE"
+  done
+fi
