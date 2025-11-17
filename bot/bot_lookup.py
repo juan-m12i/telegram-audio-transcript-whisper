3517ec85601c5aa7416db764556938a662b0eb8b
Juan Montalvo Bressi <juan-m12i@users.noreply.github.com>
2025-11-17 09:53:20 -0300

    x

--- bot/bot_lookup.py
+++ bot/bot_lookup.py
@@ -4,5 +4,6 @@ bots_lookup: Dict[str, str] = {
     "6018..0hCk": "AlfreD2",
     "6267..u8SU": "Dev",
     "5863..vl6Y": "Notes",
-    "6144..ZZEg": "MJ277"
+    "6144..ZZEg": "MJ277",
+    "8407..N3ZY": "Workout Bot"
 }
